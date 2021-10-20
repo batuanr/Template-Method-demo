af86beabe5925df81eab998e17a9663df268cc74
batuanr <tuanpesi@gmail.com>
2021-10-20 08:25:46 +0700

    xong

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Meal meal1 = new HamburgerMeal();
+        meal1.doMeal();
+
+        Meal meal2 = new TacoMeal();
+        meal2.doMeal();
+
+    }
+}
